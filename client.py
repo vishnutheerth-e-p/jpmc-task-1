f829349fd26f0d6612d0f2b0d5c086b4c6267fe2
Vishnutheerth E P <116893957+vishnutheerth-e-p@users.noreply.github.com>
2022-11-07 10:24:05 +0530

    Update client.py

--- client.py
+++ client.py
@@ -58,4 +58,4 @@ if __name__ == "__main__":
             stock, bid_price, ask_price, price = getDataPoint(quote)
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price['ABC'], price['DEF']))
+        print("Ratio %s" % getRatio(price['ABC'], price['DEF']))  #change3
